4b48c44bd2496a727fdbcf243ebf820678e0c6a6
yc.huang71 <yc.huang71@gmail.com>
2019-12-01 02:30:12 +0800

    add xfce4 slim

--- utilities.sh
+++ utilities.sh
@@ -6,7 +6,6 @@ sudo apt install -y python-pip python3-pip python3-dev python3-tk
 
 sudo apt install -y leafpad
 sudo apt install -y htop
-sudo apt install -y git
 
 sudo apt install -y openssh-server
 sudo apt install -y jq curl
@@ -15,6 +14,8 @@ sudo apt install -y chromium-browser
 sudo apt install -y vlc browser-plugin-vlc
 sudo apt install -y shutter
 sudo apt install -y kazam
+sudo apt install -y nomacs
+
 sudo apt install -y redshift-gtk
 
 #sudo apt install -y texlive-full
--- xfce4.sh
+++ xfce4.sh
@@ -2,3 +2,9 @@
 
 sudo apt install -y xfce4-terminal
 sudo apt install -y xfce4-indicator-plugin
+
+# themes: PRO-Dark-XFCE-Edition
+# https://www.pling.com/s/XFCE/p/1207818/
+
+# icons: Dark
+# https://www.pling.com/s/XFCE/p/1187019
