a4618c390faf09915937acab344fbea2dc235199
yc.huang71 <yc.huang71@gmail.com>
2019-12-05 21:55:17 +0800

    add uninstall.sh for xubuntu

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt autoremove thunderbird*
+sudo apt autoremove mousepad
+sudo apt autoremove gnome-sudoku gnome-mines
+sudo apt autoremove ristretto
+sudo apt autoremove parole
+sudo apt autoremove xfce4-notes xfce4-notes-plugin
