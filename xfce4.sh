9418847407c7024c284defbca48fe31d3161cbe2
yc.huang71 <yc.huang71@gmail.com>
2019-12-06 18:55:06 +0800

    add icon

--- xfce4.sh
+++ xfce4.sh
@@ -6,5 +6,8 @@ sudo apt install -y xfce4-indicator-plugin
 # themes: PRO-Dark-XFCE-Edition
 # https://www.pling.com/s/XFCE/p/1207818/
 
-# icons: Tela Icon Theme
+# icons: Tela
 # https://www.pling.com/s/XFCE/p/1279924
+
+# icons: Qogir
+# https://www.pling.com/s/XFCE/p/1296407/
