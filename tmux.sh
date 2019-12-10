f570a8dc03aa67e0f4fe093553102840ac6fd111
yc.huang71 <yc.huang71@gmail.com>
2019-12-10 15:13:23 +0800

    tmux.sh add some settings to bashrc

--- tmux.sh
+++ tmux.sh
@@ -7,3 +7,7 @@ cd ~
 wget https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash
 sudo mv ~/tmux.completion.bash /usr/share/bash-completion/completions/tmux
 
+# -- for terminal color (xfce4-terminal)
+echo "" >> ~/.bashrc
+echo "# tmux" >> ~/.bashrc
+echo "alias tmux='tmux -2'" >> ~/.bashrc
