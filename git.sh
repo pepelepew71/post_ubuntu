06492b035819bea49643943b3af48072f857fe0a
yc.huang <yc.huang71@gmail.com>
2020-06-03 23:46:53 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -8,3 +8,4 @@ git config --global user.email yc.huang71@gmail.com
 git config --global user.name yc.huang71
 git config --global push.default simple
 git config --global core.editor vim
+git config --global pull.rebase false
